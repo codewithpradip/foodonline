5c0b1de3a1192347d6d8a4ab016f845400468048
Pradip Thapa <pradipthapaofficial@gmail.com>
2022-09-27 20:37:17 +0545

    homepage setup, postgres configuration and .env-sample

--- foodonline_main/views.py
+++ foodonline_main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("welcome to new projets !!")
+    return render(request,'home.html')
